698202e943584d91fb9c7dc957ab66e8d1b31182
Akshay Kalola <akshaykumar.kalola@neosoftmail.com>
2022-07-12 18:25:23 +0530

    validation screen ui design done

--- app/src/main/java/com/suryodaybank/jyotiassisted/ui/fragments/aocpv/AocpvValidationFragment.java
+++ app/src/main/java/com/suryodaybank/jyotiassisted/ui/fragments/aocpv/AocpvValidationFragment.java
@@ -4,17 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.suryodaybank.jyotiassisted.R;
+import com.suryodaybank.jyotiassisted.databinding.FragmentAocpvValidationBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AocpvValidationFragment extends Fragment {
 
+    private FragmentAocpvValidationBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aocpv_validation, container, false);
+        binding = FragmentAocpvValidationBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupViews();
+    }
+
+    private void setupViews() {
+        List<String> proofList = new ArrayList<>();
+        proofList.add("Aadhaar card");
+        proofList.add("Voter ID");
+        proofList.add("Passport");
+        proofList.add("Driving License");
+        proofList.add("MGNREGA Card");
+        ArrayAdapter<String> proofAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, proofList);
+        proofAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.documentSpinner.setAdapter(proofAdapter);
     }
 }
