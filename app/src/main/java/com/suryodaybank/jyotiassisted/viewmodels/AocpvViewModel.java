0ccfca16919e6e44627f41e9889728e6faa68d6d
dell <minakshi.pandey@neosoftmail.com>
2022-07-27 09:30:09 +0530

    Personal details save API implemented

--- app/src/main/java/com/suryodaybank/jyotiassisted/viewmodels/AocpvViewModel.java
+++ app/src/main/java/com/suryodaybank/jyotiassisted/viewmodels/AocpvViewModel.java
@@ -10,10 +10,12 @@ import androidx.lifecycle.ViewModel;
 import com.suryodaybank.jyotiassisted.R;
 import com.suryodaybank.jyotiassisted.models.CRMCustDataResponseItem;
 import com.suryodaybank.jyotiassisted.models.CustomerDetailsRequest;
+import com.suryodaybank.jyotiassisted.models.CustomerSaveData;
 import com.suryodaybank.jyotiassisted.models.DataModel;
 import com.suryodaybank.jyotiassisted.models.MonthlyIncome;
 import com.suryodaybank.jyotiassisted.models.PreApprove;
 import com.suryodaybank.jyotiassisted.models.SaveIncomeRequest;
+import com.suryodaybank.jyotiassisted.models.UtilityAddressItem;
 import com.suryodaybank.jyotiassisted.repositories.AocpvRepository;
 import com.suryodaybank.jyotiassisted.utils.PreApproveStatus;
 import com.suryodaybank.jyotiassisted.utils.SingleLiveEvent;
@@ -43,6 +45,8 @@ public class AocpvViewModel extends ViewModel {
     public MutableLiveData<List<CRMCustDataResponseItem>> customerQueryLiveData = new MutableLiveData<List<CRMCustDataResponseItem>>();
 
     public SingleLiveEvent<Void> getMonthlyIncomeData = new SingleLiveEvent();
+    public SingleLiveEvent<Void> getCustomerDetails = new SingleLiveEvent();
+    public SingleLiveEvent<Void> getUtilityDetails = new SingleLiveEvent();
 
     @Inject
     public AocpvViewModel(AocpvRepository aocpvRepository) {
@@ -78,6 +82,11 @@ public class AocpvViewModel extends ViewModel {
                 t.printStackTrace();
             }
         });
+    }
+
+    private void getUtilityDetails() {
+        DataModel<UtilityAddressItem> body = new DataModel<>();
+
     }
 
     private void getPreApproveList() {
@@ -130,10 +139,11 @@ public class AocpvViewModel extends ViewModel {
         switch (index) {
             case 0:
                 //Save customer details
-                saveCustomerDetailsApi();
+                getCustomerDetails.call();
                 break;
             case 1:
                 //Save utility details
+
                 break;
             case 2:
                 //Save monthly income details
@@ -143,13 +153,28 @@ public class AocpvViewModel extends ViewModel {
 //        nextPage.call(); //Add this line to move to next screen
     }
 
-    private void saveCustomerDetailsApi() {
-
-    }
 
     public void callMonthlyExpenseApi() {
 
     }
+
+    public void callPersonalDetailAPI(CustomerSaveData customerSaveData) {
+        customerSaveData.setApplicationNo("12345681"); //TODO: Need to generate random
+        aocpvRepository.saveUserDetail(customerSaveData).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    nextPage.call();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+    }
+
 
     public void callMonthlyIncomeApi(SaveIncomeRequest saveIncomeRequest) {
         saveIncomeRequest.setApplicationNo("12345681"); //TODO: Need to generate random
